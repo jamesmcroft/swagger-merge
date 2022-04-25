e70df986ae65804b462ada82810555abcc8fba82
James Croft <jamz_c@hotmail.co.uk>
2022-04-25 11:32:07 +0100

    Updated JSON props for Swagger document info

--- src/SwaggerMerge/Swagger/SwaggerDocumentInfo.cs
+++ src/SwaggerMerge/Swagger/SwaggerDocumentInfo.cs
@@ -1,8 +1,12 @@
-﻿namespace SwaggerMerge.Swagger;
+﻿using Newtonsoft.Json;
+
+namespace SwaggerMerge.Swagger;
 
 public class SwaggerDocumentInfo
 {
+    [JsonProperty("title")]
     public string Title { get; set; }
 
+    [JsonProperty("version")]
     public string Version { get; set; }
 }
